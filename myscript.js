1df947b7bac913857f4073aa9fc781265957d5fc
Ryan <ryanrana_4@hotmail.com>
2021-09-20 03:25:39 +0100

    Added change colour and reset

--- myscript.js
+++ myscript.js
@@ -1,14 +1,39 @@
 const gridContainer = document.getElementById("grid-container");
 
-function createRows(rows, cols) {
-    gridContainer.style.setProperty('--grid-rows', rows);
-    gridContainer.style.setProperty('--grid-cols', cols);
-    for (let i = 0; i < (rows * cols); i++) {
+function createRows(size) {
+    gridContainer.style.setProperty('--grid-rows', size);
+    gridContainer.style.setProperty('--grid-cols', size);
+    for (let i = 0; i < (size * size); i++) {
         let grid = document.createElement("div");
-        
+        grid.addEventListener("mouseover", changeColour);
         gridContainer.appendChild(grid).className = "grid";
 
     }
 }
 
-createRows (32, 32);
+function changeColour(e) {
+    this.style.backgroundColor = "red";
+}
+
+
+function reset() {
+    let newSize = window.prompt("Enter new size");
+    newSize = parseInt(newSize);
+
+    if(newSize >= 101 || Number.isNaN(newSize) || newSize <= 0) {
+        alert("Please enter a number (Max: 100)");
+        newSize = 16;
+    }
+
+
+    let grid = document.getElementById("grid-container").childNodes;
+    for (let i = grid.length - 1; i >= 0; i --) {
+        grid[i].remove();
+    }
+
+    createRows(newSize);
+    
+    
+}
+
+createRows(16);
